00b3fa6a8adadf08f2e2f095f78243c13bd444d8
Thomas <venturini.thomas95@gmail.com>
2022-02-23 00:44:48 +0100

    concluso(no bonus)

---
+++ myvue/src/components/myTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Il mio nuovo progetto!</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+   name: "myTitle"
+
+   }
+        
+    
+</script>
+
+<style scoped>
+
+    h1 {
+        color:blue;
+    }
+
+</style>
